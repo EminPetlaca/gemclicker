2c23e8aceb38035b49cceb235c6ca7984b93831b
EminPetlaca <eminpetlacanpp@gmail.com>
2023-10-10 23:00:09 +0200

    gems

---
+++ res/js/main.js
@@ -0,0 +1,68 @@
+const gemMine = document.getElementById("gemMine");
+const gems = document.getElementById("gemCount");
+const gemIcon = document.getElementById("gemicon");
+const clickUpgrade = document.getElementById("upgrade");
+const autoClickerButton = document.getElementById("autoClicker");
+
+let numberOfGems = 0;
+let costOfClickUpgrade = 50;
+let clickUpgradeIncrease = 1;
+let autoClickIncrease = 0;
+let autoClickInterval;
+let autoClickerCost = 500;
+
+gemMine.onclick = () => {
+  console.log("click");
+  numberOfGems += clickUpgradeIncrease;
+  gems.innerHTML = `<img src="${gemIcon.src}" alt="Gem Icon"> ${numberOfGems}`;
+};
+
+const clickUpgradeFunction = () => {
+  if (numberOfGems >= costOfClickUpgrade) {
+    numberOfGems -= costOfClickUpgrade;
+    gems.innerHTML = `<img src="${gemIcon.src}" alt="Gem Icon"> ${numberOfGems}`;
+    clickUpgradeIncrease++;
+
+    costOfClickUpgrade += 10;
+    upgrade.innerHTML = `Upgrade gem mine: ${costOfClickUpgrade} <img src="${gemIcon.src}" alt="Gem Icon">`; 
+  }
+};
+
+
+
+clickUpgrade.onclick = clickUpgradeFunction;
+
+autoClickerButton.onclick = () => {
+  if (numberOfGems >= autoClickerCost) {
+    numberOfGems -= autoClickerCost;
+    autoClickerCost += 100;
+    autoClickerButton.innerHTML = `Upgrade autominer: ${autoClickerCost} <img src="${gemIcon.src}" alt="Gem Icon">`;
+
+    gems.innerHTML = `<img src="${gemIcon.src}" alt="Gem Icon"> ${numberOfGems}`;
+    autoClickIncrease++;
+
+    clearInterval(autoClickInterval);
+
+    autoClickInterval = setInterval(() => {
+      numberOfGems += autoClickIncrease;
+      gems.innerHTML = `<img src="${gemIcon.src}" alt="Gem Icon"> ${numberOfGems}`;
+    }, 1000);
+  }
+};
+
+
+
+
+
+
+
+
+
+function cheats() {
+  console.log("Cheats on!");
+  numberOfGems += 1000000;
+  gems.innerHTML = `<img src="${gemIcon.src}" alt="Gem Icon"> ${numberOfGems}`;
+}
+// spousteni funkce
+
+
